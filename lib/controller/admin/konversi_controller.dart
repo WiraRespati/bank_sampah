d4a6fa6af13c3c21e8b08ec13c73333b5e5d57a1
rajascreamo <rajascreamo04@gmail.com>
2024-08-17 10:02:35 +0800

    undone konversi page

--- lib/controller/admin/konversi_controller.dart
+++ lib/controller/admin/konversi_controller.dart
@@ -1,14 +1,22 @@
+import 'package:bank_sampah/models/barang_model.dart';
+import 'package:bank_sampah/services/admin/show_barang_services.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+
+import '../../models/user_model.dart';
+import '../../services/user/auth_service.dart';
 
 class KonversiController extends GetxController {
   // TextEditingController untuk mengontrol input teks pencarian user dan barang
   final TextEditingController searchUserController = TextEditingController();
   final TextEditingController searchBarangController = TextEditingController();
 
+  Rxn<List<UserModel>> listUsers = Rxn<List<UserModel>>([]);
+  Rxn<List<BarangModel>> listBarang = Rxn<List<BarangModel>>([]);
+  Rxn<UserModel> selectedUser = Rxn<UserModel>();
   // Observable untuk menyimpan hasil pencarian user dan barang
-  var searchUserResults = <String>[].obs;
-  var searchBarangResults = <String>[].obs;
+  var searchUserResults = [].obs;
+  var searchBarangResults = [].obs;
 
   // Observable untuk mengontrol status pencarian user dan barang
   var isSearchingUser = false.obs;
@@ -33,15 +41,17 @@ class KonversiController extends GetxController {
 
   // Fungsi untuk memulai pencarian user
   void startSearchUser(String query) {
-    if (query.isEmpty) {
-      clearSearchUser();
-      return;
+    if (query.isNotEmpty) {
+      isSearchingUser.value = true;
+      final result = listUsers.value!
+          .where((element) => element.nik!.contains(query))
+          .map((e) => e.nik)
+          .toList();
+      searchUserResults.assignAll(result);
+    } else {
+      isSearchingUser.value = false;
+      searchUserResults.clear();
     }
-
-    isSearchingUser.value = true;
-    searchUserResults.value = dummyUserData
-        .where((user) => user.toLowerCase().contains(query.toLowerCase()))
-        .toList();
   }
 
   // Fungsi untuk menghapus teks pencarian user
@@ -58,15 +68,18 @@ class KonversiController extends GetxController {
 
   // Fungsi untuk memulai pencarian barang
   void startSearchBarang(String query) {
-    if (query.isEmpty) {
-      clearSearchBarang();
-      return;
+    if (query.isNotEmpty) {
+      isSearchingBarang.value = true;
+      final result = listBarang.value!
+          .where((element) =>
+              element.name!.toLowerCase().contains(query.toLowerCase()))
+          .map((e) => e.name)
+          .toList();
+      searchBarangResults.assignAll(result);
+    } else {
+      isSearchingBarang.value = false;
+      searchBarangResults.clear();
     }
-
-    isSearchingBarang.value = true;
-    searchBarangResults.value = dummyBarangData
-        .where((barang) => barang.toLowerCase().contains(query.toLowerCase()))
-        .toList();
   }
 
   // Fungsi untuk menghapus teks pencarian barang
@@ -79,5 +92,40 @@ class KonversiController extends GetxController {
   void clearSearchBarang() {
     isSearchingBarang.value = false;
     searchBarangResults.clear();
+  }
+
+  void getAllUser() async {
+    try {
+      final response = await AuthService().getAllUserData();
+      listUsers.value = response.values
+          .map((doc) => UserModel.fromJson(doc as Map<String, dynamic>))
+          .toList();
+    } catch (e) {
+      Get.snackbar(
+        'Error',
+        'Gagal mengambil data user (error: $e)',
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
+
+  void getAllBarang() async {
+    try {
+      final response = await ShowBarangServices().getAllBarangData();
+      listBarang.value = response['barang'];
+    } catch (e) {
+      Get.snackbar(
+        'Error',
+        'Gagal mengambil data barang (error: $e)',
+        snackPosition: SnackPosition.TOP,
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+      );
+    }
+  }
+
+  void getDataUser(String nik) {
+    final user = listUsers.value!.firstWhere((element) => element.nik == nik);
+    selectedUser.value = user;
   }
 }
