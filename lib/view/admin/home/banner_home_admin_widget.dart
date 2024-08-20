fdff58a628eda2af05bce03e2d71d0fc365157cf
Wira Respati <wirarespatibppn@gmail.com>
2024-08-20 11:39:17 +0800

    add widget for point all user on home admin page

--- lib/view/admin/home/banner_home_admin_widget.dart
+++ lib/view/admin/home/banner_home_admin_widget.dart
@@ -1,4 +1,5 @@
 import 'package:bank_sampah/utils/color_constant.dart';
+import 'package:bank_sampah/utils/text_style_constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -8,62 +9,182 @@ class BannerHomeAdminWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 275,
+      height: 390,
       width: double.infinity,
-      child: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 70),
-            child: ClipRRect(
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(12),
-                bottomRight: Radius.circular(12),
-              ),
-              child: Image.asset(
-                'assets/images/banner_home.png',
-                width: double.infinity,
-              ),
+      child: Stack(children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 70),
+          child: ClipRRect(
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(12),
+              bottomRight: Radius.circular(12),
+            ),
+            child: Image.asset(
+              'assets/images/banner_home.png',
+              width: double.infinity,
             ),
           ),
-          Container(
-            height: 90,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: ColorCollection.white,
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
+        ),
+        Container(
+          height: 90,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: ColorCollection.white,
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
             ),
-            child: Row(
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/logobs.png',
+                      height: 40,
+                      width: 40,
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    SizedBox(
+                      height: 37,
+                      width: 174,
+                      child: SvgPicture.asset(
+                        'assets/images/title_bank_sampah.svg',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          top: 250,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: Stack(
               children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 18),
-                  child: Row(
-                    children: [
-                      Image.asset(
-                        'assets/images/logobs.png',
-                        height: 40,
-                        width: 40,
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      SizedBox(
-                        height: 37,
-                        width: 174,
-                        child: SvgPicture.asset(
-                          'assets/images/title_bank_sampah.svg',
-                        ),
+                Container(
+                  padding: const EdgeInsets.only(
+                    left: 18,
+                    right: 2,
+                  ),
+                  height: 140,
+                  width: 320,
+                  decoration: BoxDecoration(
+                    color: ColorNeutral.neutral50,
+                    border: Border.all(
+                      color: const Color(0xFFB6E0B1),
+                      width: 1,
+                    ),
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: const Color(0xFFB6E0B1).withOpacity(0.25),
+                        offset: const Offset(0, 4),
+                        blurRadius: 20,
+                        spreadRadius: 0,
                       ),
                     ],
                   ),
+                  child: Padding(
+                      padding: const EdgeInsets.only(top: 18),
+                      child: Text(
+                        "Halo Admin Selamat Datang",
+                        style: TextStyleCollection.captionMedium.copyWith(
+                          fontSize: 16,
+                          color: ColorPrimary.primary100,
+                        ),
+                      )),
                 ),
+                Positioned(
+                  top: 55,
+                  child: Container(
+                    height: 85,
+                    width: 320,
+                    decoration: BoxDecoration(
+                      color: ColorNeutral.neutral50,
+                      border: Border.all(
+                        color: const Color(0xFFB6E0B1),
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color(0xFF000000).withOpacity(0.25),
+                          offset: const Offset(0, 4),
+                          blurRadius: 20,
+                          spreadRadius: 0,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 18,
+                          ),
+                          child: Text(
+                            "Total Point User",
+                            style: TextStyleCollection.bodyBold.copyWith(
+                              fontSize: 12,
+                              color: ColorPrimary.primary100,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        const Divider(
+                          color: Color(0xFFB6E0B1),
+                          thickness: 1,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 18,
+                          ),
+                          child: Row(
+                            children: [
+                              Image.asset(
+                                'assets/images/koin.png',
+                                height: 25,
+                                width: 25,
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                "0",
+                                style:
+                                    TextStyleCollection.captionMedium.copyWith(
+                                  fontSize: 16,
+                                  color: ColorCollection.black,
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
               ],
             ),
           ),
-        ],
-      ),
+        ),
+      ]),
     );
   }
 }
